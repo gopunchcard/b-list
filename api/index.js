6abe9a8df6112e58595f778ab83016a4d4b829a2
mark-zacharias <mark@zacharias.me>
2016-12-23 15:43:50 -0700

    save mac detections

--- api/index.js
+++ api/index.js
@@ -2,28 +2,33 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var storage = require('node-persist');
+//var Q = require('Q');
+
+var upload = multer(); // for parsing multipart/form-data
+var app = express();
 
 function init(){
-    var upload = multer(); // for parsing multipart/form-data
-    var app = express();
     app.use(bodyParser.json()); // for parsing application/json
     app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
     storage.init()
     .then(setupRouting)
-    .then(startListener);
+    .then(startListener)
+    .catch(logError);
 }
 function setupRouting(){
     app.get('/', helloWorld);
-
     app.get('/users', getUsers);
-
     app.post('/bluetooth', upload.array(), saveBluetoothDection);
+
+    return;
 }
 function startListener(){
     app.listen(3010, function(){
         console.log('app listening on pot 3010!');
     });
+
+    return;
 }
 
 function helloWorld(req, res){
@@ -33,11 +38,44 @@ function getUsers(req, res){
     res.json([{name: "test", mac: "123", room: "1", date: "2016-12-12"}]);
 }
 function saveBluetoothDection (req, res, next) {
-    console.log(req.body);
-    res.json(req.body);
 
-    storage.setItem('mac', req.body.mac)
+    getRecord(req.body.mac)
+    .then(function(record){return createUpdateRecord(record, req.body.mac);})
+    .then(saveRecord)
+    .then(function(){
+        console.info('save success');
+        res.send('save success');
+    })
+    .catch(function(err){console.error('save fail for', req.body.mac, err);});
+
+}
+function getRecord(mac){
+    return storage.getItem(mac);
+}
+function createUpdateRecord(record, mac){
+    if(!record){
+        record = {};
+    }
+    record.mac = mac;
+    record.time = new Date();
+    
+    return record;
+}
+function saveRecord(record){
+    return storage.setItem(record.mac, record);
 }
 
+function logError(err){
+    console.error(err);
+}
+
+function storageTest(){
+    storage.setItem('mac', '1234').then(function(){
+        return storage.getItem('name');
+    })
+    .then(function(value){
+        console.log(value);
+    });
+}
 
 init();
