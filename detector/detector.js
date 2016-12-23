2e721b60e14c68bfdbfe6cb08d46ec81e16f28de
Terence Leung <terence@punchcard.io>
2016-12-23 16:03:51 -0700

    python implementation for detector

--- detector/detector.js
+++ detector/detector.js
@@ -1,11 +1,20 @@
 var noble = require('noble');
+//var Scanner = require('bluetooth-scanner');
+var xhr = require('xhr');
 
-noble.startScanning();
+noble.on('stateChange', function (state) {
+	if (state === 'poweredOn') {
+		setInterval(noble.startScanning(), 60000);
+		console.log("Scanning...")
+	} else {
+		noble.stopScanning();
+	}
+});
 
-noble.on(‘discover’, function(peripheral) { 
-
-  var macAddress = peripheral.uuid;
-  var rss = peripheral.rssi;
-  var localName = advertisement.localName; 
-  console.log('found device: ', macAdress, ' ', localName, ' ', rss);   
-}
+noble.on('discover', function (peripheral) {
+	var macAddress = peripheral.address;
+	console.log('found device: ', macAddress);
+	xhr.post('http://localhost:3010/bluetooth', function (err, resp) {
+		console.log(resp.body)
+	})
+});
