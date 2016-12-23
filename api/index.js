202b8863a274867009997b201bb0aad9d302d5de
mark-zacharias <mark@zacharias.me>
2016-12-23 16:03:27 -0700

    add cors to api to allow requests from web site

--- api/index.js
+++ api/index.js
@@ -8,6 +8,11 @@ var upload = multer(); // for parsing multipart/form-data
 var app = express();
 
 function init(){
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.use(bodyParser.json()); // for parsing application/json
     app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
