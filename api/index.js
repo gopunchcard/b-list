90bd41187de17f80bfc818d97507b69828ec5436
mark-zacharias <mark@zacharias.me>
2016-12-23 13:38:42 -0700

    stub for bluetooth detection post

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
 var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function(req, res){
     res.send('Hello World');
+});
+
+app.post('/bluetooth', upload.array(), function (req, res, next) {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(3010, function(){
