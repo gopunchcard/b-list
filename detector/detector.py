fc4e0b342a390ae083c874dbcc6dec8136134f4b
Terence Leung <terence@punchcard.io>
2016-12-23 17:23:57 -0700

    merged

--- detector/detector.py
+++ detector/detector.py
@@ -1,3 +1,4 @@
+#from bluetooth.ble import DiscoveryService
 import bluetooth
 import json
 import requests
@@ -7,10 +8,15 @@ url = 'http://10.0.1.184:3010/bluetooth'
 
 while True:
 	nearby_devices = bluetooth.discover_devices(duration=20, lookup_names=True)
+	print("found %d devices" % len(nearby_devices))
 	for addr, name in nearby_devices:
 		payload = { 'mac': str(addr) }
 		headers = {'content-type': 'application/json'}
 		requests.post(url, data=json.dumps(payload), headers=headers)
 
 	time.sleep(60)
+	# service = DiscoveryService()
+	# devices = service.discover(2)
+	# for address, name in devices.items():
+		# print("name: {}, address: {}".format(name, address))
 
